09b1e72f0ab1c7e09ec3c442d0d0dfe4fd08a678
RidiculousBuffal <3322946829@qq.com>
2024-09-19 01:01:31 +0800

    update cathay logo

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -1,6 +1,7 @@
-export const BRANDING_NAME = 'LobeChat';
+export const BRANDING_NAME = 'CathayBot';
 
 export const ORG_NAME = 'lobehub';
 
 export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_LOGO_URL =
+  'https://hpcow-1316827225.cos.ap-shanghai.myqcloud.com/cathayLogo.svg';
